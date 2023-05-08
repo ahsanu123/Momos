89d526f8a4ddcab14912f09aacab3a2bbaca67fb
ahsanu123 <caasperahsanuamala5@gmail.com>
2023-05-08 04:50:11 -0700

    backup 8 mei 2023

--- gradioUI.py
+++ gradioUI.py
@@ -9,12 +9,19 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import serial
 import serial.tools.list_ports as serPorts
 import threading
 import glob
-# from multiprocessing import Process
-# import queue 
+
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import math
+from scipy.fft import fft, fftfreq
+
 
 import time
 import schedule # not built in library
@@ -25,94 +32,319 @@ from datetime import datetime
 import psutil
 
 import gradio as gr
+gr.close_all()
+
+# ==========================================================================
+# Global Variable Section 
+# ==========================================================================
+latestFile = ''
+# fig = go.Figure()
+# fig.add_scatter(x=x, y=y, mode="lines", name ="fft")
 
 
 # ==========================================================================
 # Class section
 # ==========================================================================
 
-# class ReadLine:
-#     def __init__(self, s):
-#         self.buf = bytearray()
-#         self.s = s
-    
-#     def readline(self):
-#         i = self.buf.find(b"\n")
-#         if i >= 0:
-#             r = self.buf[:i+1]
-#             self.buf = self.buf[i+1:]
-#             return r
-#         while True:
-#             i = max(1, min(2048, self.s.in_waiting))
-#             data = self.s.read(i)
-#             i = data.find(b"\n")
-#             if i >= 0:
-#                 r = self.buf + data[:i+1]
-#                 self.buf[0:] = data[i+1:]
-#                 return r
-#             else:
-#                 self.buf.extend(data)
                 
 
 # ==========================================================================
 # Function section
 # ==========================================================================
+plot_end = 2 * math.pi
 
-# def serialPrint(inqueue):
-#     global ser
-#     print("Hallow??")
-#     while True:
-#         #buf = str(ser.readline())
-#         inqueue.put("buf")
 
-# def startProcess():
-#     #set_start_method("spawn")
-#     Process(target=serialPrint).start()
-#     return 
-
-# def connectPort(port, baudrate):
-#     global ser
-#     global boolPrintSerial
-#     global getDataBtn
-    
-    
-#     if port=='' or baudrate=='': 
-#         return "## ⚠ Select Port and Baudrate!!!"
-#     try:
-#         ser = serial.Serial(port=port, baudrate=baudrate)
-#         ser=ser
-        
-#     except serial.serialutil.SerialException as e:
-#         boolPrintSerial=False
-#         return "❌ port NOT Connected: " + str(port) + str(e) 
-    
-    
-#     boolPrintSerial=True
-#     print(ser.is_open)
-#     return "✅ port Connected: " + str(port) + "\ninfo: " + str(ser) 
-
-def plot_forecast(final_year, companies, noise, show_legend, point_style):
-    start_year = 2020
-    x = np.arange(start_year, final_year + 1)
-    year_count = x.shape[0]
-    plt_format = ({"cross": "X", "line": "-", "circle": "o--"})[point_style]
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for i, company in enumerate(companies):
-        series = np.arange(0, year_count, dtype=float)
-        series = series**2 * (i + 1)
-        series += np.random.rand(year_count) * noise
-        ax.plot(x, series, plt_format)
-    if show_legend:
-        plt.legend(companies)
+def get_plot(period=1):
+    global plot_end
+    x = np.arange(plot_end - 2 * math.pi, plot_end, 0.02)
+    y = np.sin(2*math.pi*period * x)
+    fig = px.line(x=x, y=y)
+    plot_end += 2 * math.pi
+    if plot_end > 1000:
+        plot_end = 2 * math.pi
     return fig
 
-def plot_random():
-    y = np.random.rand(100)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(range(0,100), y)
+def currentPlot():
+    global latestFile
+    data = pd.read_csv(latestFile)
+    data.columns = ['time', 'ia', 'ib', 'ic' ,'vref', 'vtemp', 'x', 'y', 'z', 'temp' ]
+    
+    ia = data['ia']
+    ib = data['ib']
+    ic = data['ic']
+    
+    ia = np.array(ia)
+    ib = np.array(ib)
+    ic = np.array(ic)
+    ia = ia/4095.0 *3.3
+    ib = ib/4095.0 *3.3
+    ic = ic/4095.0 *3.3
+    
+    # IA =============================
+    iaffty = fft(ia, norm="forward")
+    iafftx = fftfreq(len(ia), 1.0/100.0)
+    
+    iaffty = np.abs(iaffty[0:len(iaffty)//2])
+    iafftx = iafftx[0:len(iafftx)//2] 
+    
+    # IB =============================
+    ibffty = fft(ib, norm="forward")
+    ibfftx = fftfreq(len(ib), 1.0/100.0)
+    
+    ibffty = np.abs(ibffty[0:len(ibffty)//2])
+    ibfftx = ibfftx[0:len(ibfftx)//2] 
+    
+    # IC =============================
+    icffty = fft(ic, norm="forward")
+    icfftx = fftfreq(len(ic), 1.0/100.0)
+    
+    icffty = np.abs(icffty[0:len(icffty)//2])
+    icfftx = icfftx[0:len(icfftx)//2] 
+    
+    fig = make_subplots(rows=3, cols=2)
+    
+    
+    # make figure of each signal
+    figIA = go.Scatter(x=iafftx, y=iaffty, mode="lines", name="IA")
+    figIB = go.Scatter(x=ibfftx, y=ibffty, mode="lines", name="IB")
+    figIC = go.Scatter(x=icfftx, y=icffty, mode="lines", name="IC")
+    figOriIA = go.Scatter(x=np.arange(0, len(ia)), y=ia, mode="lines", name="time IA")
+    figOriIB = go.Scatter(x=np.arange(0, len(ib)), y=ib, mode="lines", name="time IB")
+    figOriIC = go.Scatter(x=np.arange(0, len(ic)), y=ic, mode="lines", name="time IC")
+    
+    fig.append_trace(figIA, row=1, col=1)
+    fig.append_trace(figIB, row=2, col=1)
+    fig.append_trace(figIC, row=3, col=1)
+    
+    fig.append_trace(figOriIA, row=1, col=2)
+    fig.append_trace(figOriIB, row=2, col=2)
+    fig.append_trace(figOriIC, row=3, col=2)
+    
+    fig['layout']['xaxis']['title']='Freq (Hz)'
+    fig['layout']['xaxis2']['title']='time (ms)'
+    fig['layout']['xaxis3']['title']='Freq (Hz)'
+    fig['layout']['xaxis4']['title']='time (ms)'
+    fig['layout']['xaxis5']['title']='Freq (Hz)'
+    fig['layout']['xaxis6']['title']='time (ms)'
+    
+    fig['layout']['yaxis']['title']='IA amplitude'
+    fig['layout']['yaxis2']['title']='IA amplitude'
+    fig['layout']['yaxis3']['title']='IB amplitude'
+    fig['layout']['yaxis4']['title']='IB amplitude'
+    fig['layout']['yaxis5']['title']='IC amplitude'
+    fig['layout']['yaxis6']['title']='IC amplitude'
+    
+    fig.update_layout(autosize=True,width=1200,height=900)
+    
     return fig
+
+
+def vibrationPlot():
+    global latestFile
+    data = pd.read_csv(latestFile)
+    data.columns = ['time', 'ia', 'ib', 'ic' ,'vref', 'vtemp', 'x', 'y', 'z', 'temp' ]
+    
+    vibx = data['x']
+    viby = data['y']
+    vibz = data['z']
+    
+    vibx = np.array(vibx)
+    viby = np.array(viby)
+    vibz = np.array(vibz)
+    
+    # VibX =============================
+    vibxffty = fft(vibx, norm="forward")
+    vibxfftx = fftfreq(len(vibx), 1.0/100.0)
+    
+    vibxffty = np.abs(vibxffty[0:len(vibxffty)//2])
+    vibxfftx = vibxfftx[0:len(vibxfftx)//2] 
+    
+    # VibY =============================
+    vibyffty = fft(viby, norm="forward")
+    vibyfftx = fftfreq(len(viby), 1.0/100.0)
+    
+    vibyffty = np.abs(vibyffty[0:len(vibyffty)//2])
+    vibyfftx = vibyfftx[0:len(vibyfftx)//2] 
+    
+    # VibZ =============================
+    vibzffty = fft(vibz, norm="forward")
+    vibzfftx = fftfreq(len(vibz), 1.0/100.0)
+    
+    vibzffty = np.abs(vibzffty[0:len(vibzffty)//2])
+    vibzfftx = vibzfftx[0:len(vibzfftx)//2] 
+    
+    fig = make_subplots(rows=3, cols=2)
+    
+    
+    # make figure of each signal
+    figVibX = go.Scatter(x=vibxfftx, y=vibxffty, mode="lines", name="VibX")
+    figVibY = go.Scatter(x=vibyfftx, y=vibyffty, mode="lines", name="VibY")
+    figVibZ = go.Scatter(x=vibzfftx, y=vibzffty, mode="lines", name="VibZ")
+    figOriVibx = go.Scatter(x=np.arange(0, len(vibx)), y=vibx, mode="lines", name="time Vibx")
+    figOriViby = go.Scatter(x=np.arange(0, len(viby)), y=viby, mode="lines", name="time Viby")
+    figOriVibz = go.Scatter(x=np.arange(0, len(vibz)), y=vibx, mode="lines", name="time Vibz")
+    
+    fig.append_trace(figVibX, row=1, col=1)
+    fig.append_trace(figVibY, row=2, col=1)
+    fig.append_trace(figVibZ, row=3, col=1)
+    
+    fig.append_trace(figOriVibx, row=1, col=2)
+    fig.append_trace(figOriViby, row=2, col=2)
+    fig.append_trace(figOriVibz, row=3, col=2)
+    
+    fig['layout']['xaxis']['title']='Freq (Hz)'
+    fig['layout']['xaxis2']['title']='time (ms)'
+    fig['layout']['xaxis3']['title']='Freq (Hz)'
+    fig['layout']['xaxis4']['title']='time (ms)'
+    fig['layout']['xaxis5']['title']='Freq (Hz)'
+    fig['layout']['xaxis6']['title']='time (ms)'
+    
+    fig['layout']['yaxis']['title']='VibX amplitude'
+    fig['layout']['yaxis2']['title']='VibX amplitude'
+    fig['layout']['yaxis3']['title']='VibY amplitude'
+    fig['layout']['yaxis4']['title']='VibY amplitude'
+    fig['layout']['yaxis5']['title']='VibZ amplitude'
+    fig['layout']['yaxis6']['title']='VibZ amplitude'
+    
+    fig.update_layout(autosize=True,width=1200,height=900)
+    
+    return fig
+    
+
+def preprocessing():
+    global latestFile
+    # global fig
+    
+    data = pd.read_csv(latestFile)
+    data.columns = ['time', 'ia', 'ib', 'ic' ,'vref', 'vtemp', 'x', 'y', 'z', 'temp' ]
+    
+    ia = data['ia']
+    ib = data['ib']
+    ic = data['ic']
+    vibx = data['x']
+    viby = data['y']
+    vibz = data['z']
+    temp = data['temp']
+    
+    ia = np.array(ia)
+    ib = np.array(ib)
+    ic = np.array(ic)
+    ia = ia/4095.0 *3.3
+    ib = ib/4095.0 *3.3
+    ic = ic/4095.0 *3.3
+    
+    vibx = np.array(vibx)
+    viby = np.array(viby)
+    vibz = np.array(vibz)
+    temp = np.array(temp)
+    
+    """
+    calculate each of fft from signal
+    """
+    # IA =============================
+    iaffty = fft(ia, norm="forward")
+    iafftx = fftfreq(len(ia), 1.0/100.0)
+    
+    iaffty = np.abs(iaffty[0:len(iaffty)//2])
+    iafftx = iafftx[0:len(iafftx)//2] 
+    
+    # IB =============================
+    ibffty = fft(ib, norm="forward")
+    ibfftx = fftfreq(len(ib), 1.0/100.0)
+    
+    ibffty = np.abs(ibffty[0:len(ibffty)//2])
+    ibfftx = ibfftx[0:len(ibfftx)//2] 
+    
+    # IC =============================
+    icffty = fft(ic, norm="forward")
+    icfftx = fftfreq(len(ic), 1.0/100.0)
+    
+    icffty = np.abs(icffty[0:len(icffty)//2])
+    icfftx = icfftx[0:len(icfftx)//2] 
+    
+    # VibX =============================
+    vibxffty = fft(vibx, norm="forward")
+    vibxfftx = fftfreq(len(vibx), 1.0/100.0)
+    
+    vibxffty = np.abs(vibxffty[0:len(vibxffty)//2])
+    vibxfftx = vibxfftx[0:len(vibxfftx)//2] 
+    
+    # VibY =============================
+    vibyffty = fft(viby, norm="forward")
+    vibyfftx = fftfreq(len(viby), 1.0/100.0)
+    
+    vibyffty = np.abs(vibyffty[0:len(vibyffty)//2])
+    vibyfftx = vibyfftx[0:len(vibyfftx)//2] 
+    
+    # VibZ =============================
+    vibzffty = fft(vibz, norm="forward")
+    vibzfftx = fftfreq(len(vibz), 1.0/100.0)
+    
+    vibzffty = np.abs(vibzffty[0:len(vibzffty)//2])
+    vibzfftx = vibzfftx[0:len(vibzfftx)//2] 
+    
+    """
+    calculate Parks vector
+    """
+    
+    parksId = np.sqrt(2./3 * ia) - 1./np.sqrt(6) * ib - 1./np.sqrt(6) * ic
+    parksIq = 1./np.sqrt(2) * ib - 1./np.sqrt(2) * ic
+    
+    fig = make_subplots(rows=3, cols=3)
+    
+    
+    # make figure of each signal
+    figIA = go.Scatter(x=iafftx, y=iaffty, mode="lines", name="IA")
+    figIB = go.Scatter(x=ibfftx, y=ibffty, mode="lines", name="IB")
+    figIC = go.Scatter(x=icfftx, y=icffty, mode="lines", name="IC")
+    figVibX = go.Scatter(x=vibxfftx, y=vibxffty, mode="lines", name="VibX")
+    figVibY = go.Scatter(x=vibyfftx, y=vibyffty, mode="lines", name="VibY")
+    figVibZ = go.Scatter(x=vibzfftx, y=vibzffty, mode="lines", name="VibZ")
+    parks = go.Scatter(x=parksId, y=parksIq, mode="lines", name="Park's")
+    
+    fig.append_trace(figIA, row=1, col=1)
+    fig.append_trace(figIB, row=1, col=2)
+    fig.append_trace(figIC, row=1, col=3)
+    
+    fig.append_trace(figVibX, row=2, col=1)
+    fig.append_trace(figVibY, row=2, col=2)
+    fig.append_trace(figVibZ, row=2, col=3)
+    
+    fig.append_trace(parks, row=3, col=1)
+    
+    fig['layout']['xaxis']['title']='Freq (Hz)'
+    fig['layout']['xaxis2']['title']='Freq (Hz)'
+    fig['layout']['xaxis3']['title']='Freq (Hz)'
+    
+    fig['layout']['yaxis']['title']='IA amplitude'
+    fig['layout']['yaxis2']['title']='IB amplitude'
+    fig['layout']['yaxis3']['title']='IC amplitude'
+    
+    fig['layout']['xaxis4']['title']='Freq (Hz)'
+    fig['layout']['xaxis5']['title']='Freq (Hz)'
+    fig['layout']['xaxis6']['title']='Freq (Hz)'
+    
+    fig['layout']['yaxis4']['title']='Vibration X'
+    fig['layout']['yaxis5']['title']='Vibration Y'
+    fig['layout']['yaxis6']['title']='Vibration Z'
+    
+    fig['layout']['xaxis7']['title']='id'
+    
+    fig['layout']['yaxis7']['title']='iq'
+    
+    fig.update_layout(autosize=False,width=1200,height=900)
+    
+    
+    return fig
+
+def updatePreprocessing():
+    global fig
+    
+    x = [1,2,3,4,5,6,7,8,9]
+    y = np.random.rand(9)
+    
+    fig.data[0].x=x
+    fig.data[0].y=y
 
 def listPort():
     buffer=''
@@ -153,15 +385,15 @@ def startSchedule(t, com):
 
 """
 TODO: 
-    1. buat fungsi dengan multiprocessing untuk mengambil data dari serial
-    2. buat fungsi untuk melakukan plot, preprocessing dan menampilkan data input untuk inference
+    1. buat fungsi dengan multiprocessing untuk mengambil data dari serial ✔️
+    2. buat fungsi untuk melakukan plot, preprocessing dan menampilkan data input untuk inference 
     3. buat plot untuk menampilkan hasil kemungkinan dari model
     4. buat fungsi untuk menampilkan hasil klasifikasi (sehat,BRB,dll)
-    5. buat fungsi untuk setting, seperti com port baudrate dsb
+    5. buat fungsi untuk setting, seperti com port baudrate dsb ✔️
     6. [optional] buat fungsi untuk menampilkan informasi model 
-    7. buat fungsi untuk menampilkan hasil pembacaan dari sensor
+    7. buat fungsi untuk menampilkan hasil pembacaan dari sensor 
     8. [optional] buat fungsi untuk menampilkan history request dari modbus
-    9. buat fungsi untuk mengganti seting modbus
+    9. buat fungsi untuk mengganti seting modbus ✔
 """
 
 
@@ -169,10 +401,34 @@ TODO:
 # Block user interface
 # ==========================================================================
 
-bl = gr.Blocks()
+bl = gr.Blocks(title="MoMoS - Motor Monitoring system")
 with bl:
     gr.Markdown("# MoMos - Motor Monitoring System Dashboard")    
-    with gr.Tab("Setting"):
+    
+    with gr.Tab("STATUS"):
+        gr.Markdown("# Status")
+    
+    with gr.Tab("DASHBOARDS"):
+        gr.Markdown("# Dashboard")
+        
+        with gr.Column():
+            gr.Markdown("## Time Series ⌚ ")
+            up=gr.Button(value="Update From Last Data")
+            plotCur1 = gr.Plot(label="Dashboard 1")
+            
+            gr.Markdown("### Current")
+            plotCur2 = gr.Plot(label="Current Plot")
+            
+            gr.Markdown("### Vibration")
+            plotCur3 = gr.Plot(label="Vibration Plot")
+            
+            bl.load(preprocessing, None, plotCur1)
+            up.click(preprocessing, None, plotCur1)
+            repeat1 = bl.load(currentPlot, None, plotCur2, every=6)
+            repeat2 = bl.load(vibrationPlot, None, plotCur3, every=6)
+                
+    
+    with gr.Tab("SETTING"):
         with gr.Column():
             gr.Markdown("## Port Settings: ")
             portListMd = gr.Markdown()
@@ -182,7 +438,7 @@ with bl:
                 intime = gr.Number(label="Every", value=5, interactive=True)
             
             
-            startStatus = gr.Markdown(value="yoyoyo")
+            startStatus = gr.Markdown(value="no info")
             with gr.Row():
                 listPortBtn = gr.Button(value="List Port")
                 startBtn = gr.Button(value="Start")
@@ -193,57 +449,28 @@ with bl:
             
             
             # modbus section 
-            gr.Markdown("## modbus setting: ")
-            portRadio = gr.Radio(['1', '2', '3', '4', '5'], label="Choose Modbus NUM")
-            gr.Textbox(max_lines=1, label="MODBUS NUM: ", placeholder="Choose or Insert")
+            # gr.Markdown("## modbus setting: ")
+            # portRadio = gr.Radio(['1', '2', '3', '4', '5'], label="Choose Modbus NUM")
+            # gr.Textbox(max_lines=1, label="MODBUS NUM: ", placeholder="Choose or Insert")
             
-    with gr.Tab("Display"):
-        with gr.Column():
-            gr.Markdown("### display plot Here!!")
-            year = gr.Radio([2025, 2030, 2035, 2040], label="Project to:")
-            group = gr.CheckboxGroup(["Google", "Microsoft", "Gradio"], label="Company Selection")
-            noise = gr.Slider(1, 100, label="Noise Level")
-            showLegend = gr.Checkbox(label="Show Legend")
-            style = gr.Dropdown(["cross", "line", "circle"], label="Style")
-            
-            showPlot = gr.Button("Plot")
-            
-            plotPlace = gr.Plot(label="forecast")
-            
-            showPlot.click(plot_forecast,inputs=[year,group,noise,showLegend,style],outputs=plotPlace)
-            
-    with gr.Tab("Clasification"):
-        gr.Markdown("# Do Clasification here")
-        
-        with gr.Row():
-            with gr.Column():
-                gr.Markdown("## show current image data here")
-                randomPlot = gr.Plot(label="current data")
-                gr.Markdown("## "+str(datetime.now()))
-                
-                
-                bl.load(plot_random,outputs=randomPlot)
-                
-            with gr.Column():
-                gr.Markdown("## display prediction bar here")
-                predictionBar = gr.Plot()
-                predictionResult = gr.Plot()
-                bl.load(plot_random,outputs=predictionBar)
-                bl.load(plot_random,outputs=predictionResult)
-
-
+    
 def startGradio():
-    print("Star Gradio Server")
-    bl.launch(server_port=8070, debug=True)
+    print("=============================")
+    print("Start Gradio Server")
+    print("=============================")
+    bl.queue().launch(server_port=7070, debug=True)
 
 def main():
+    global latestFile
+    
     schedule.clear()
     while True:
         schedule.run_pending()
         dirname = os.path.join(os.path.dirname(__file__), 'csv','*') 
         listFile = glob.glob(dirname)
         lastFile = max(listFile, key=os.path.getmtime)
-        print(lastFile)
+        latestFile = lastFile
+        
         
         
         
@@ -252,6 +479,7 @@ gradioThread.start()
 
 print("Starting Main Program!!")
 main()
+# startGradio()
     
 
     
